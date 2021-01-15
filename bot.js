efb810cfe5ee399d5f958f189c1a3ab4f07d7e1f
Berker <68822081+berker00@users.noreply.github.com>
2021-01-15 10:43:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,69 +11,14 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content.toLowerCase === 'hav') {
-    msg.reply('host aminakodumunun');
+  if (msg.content.toLowerCase === '56') {
+    msg.reply('author');
   }
   if (!msg.content.startsWith(prefix)) {
     return;
   }
   if(msg.content.toLowerCase() === prefix + `author`) {
     msg.reply(`Berker Keskin`)
-  }
-  if(msg.content.toLowerCase() === prefix + `napiyon knk`) {
-    msg.reply(`Berkere soyleme de canim cok sikkin`)
-  }
-  if(msg.content.toLowerCase() === prefix + `noldu`) {
-    msg.reply(`bu Berker benden habersiz kariya gidiyor beni cagirmiyor sikicem onu`)
-  } 
-  if(msg.content.toLowerCase() === prefix + `fatih`) {
-    msg.reply(`https://www.youtube.com/watch?v=xzUqc--CVjY`)
-  }
-  if(msg.content.toLowerCase() === prefix + `enes`) {
-    msg.reply(`AKSUNUCUM ABI https://aksunucum.com`)
-  }
-  if(msg.content.toLowerCase() === prefix + `doruk`) {
-    msg.reply(`Buyur Birader Ben Abisiyim https://tenor.com/view/hayırdır-birader-hayırdır-bilader-bir-sorun-mu-var-dog-köpek-gif-19119669`)
-  }
-  if(msg.content.toLowerCase() === prefix + `berker`) {
-    msg.reply(`Ulu onder, gazi , maresal , son hava bukucu , padisah , sultanlar sultani , sadrazamin sol ve sag tasagi , ayni zamanda CAN YAMAN Berker mi?`)
-  }
-  if(msg.content.toLowerCase() === prefix + `yagiz`) {
-    msg.reply(`Bunu mu demek istemistiniz: OROSPU`)
-  }
-  if(msg.content.toLowerCase() === prefix + `ibo`) {
-    msg.reply(`https://youtu.be/BUxCUdWYKgg`)
-  }
-  if(msg.content.toLowerCase() === prefix + `arif`) {
-    msg.reply(`BU MESAJA CEVAP VEREMIYORUM OROSBU COCUKLARININ DDOS SALDIRISI ALTINDAYIZ`)
-  }
-  if(msg.content.toLowerCase() === prefix + `emre`) {
-    msg.reply(`ONU BUNU BILMEM AGA BEN BAROTRAUMACIYIM : https://store.steampowered.com/app/602960/Barotrauma/`)
-  }
-  if(msg.content.toLowerCase() === prefix + `davet`) {
-    msg.reply(`https://discord.com/oauth2/authorize?client_id=798950239834865704&scope=bot&permissions=805829694`)
-  }
-  if(msg.content.toLowerCase() === prefix + `help`) {
-    msg.reply(`?kisi adi yazarak cevaplari gorebilirsiniz,
-    ?davet yazarak sunucunuza davet linkini gorebilirsiniz.`)
-  }
-  if(msg.content.toLowerCase() === prefix + `emir`) {
-    msg.reply(`KOR PIC`)
-  }
-  if(msg.content.toLowerCase() === prefix + `` ) {
-    msg.reply(`anlasilmadi lutfen duzgun yaziniz | ORNEK: ?komut |`)
-  }
-  if(msg.content.toLowerCase() === prefix + `burak`) {
-    msg.reply(`ne var la bebe`)
-  }
-  if(msg.content.toLowerCase() === prefix + `zey`) {
-    msg.reply( `miyav?`)
-  }
-  if(msg.content.toLowerCase() === prefix + `ahmet`) {
-    msg.reply(`THIS IS MALATYAAAAAAAA`)
-  }
-  if(msg.content.toLowerCase() === prefix + `altug`) {
-    msg.reply( `800K KAYN`)
   }
 });
 
